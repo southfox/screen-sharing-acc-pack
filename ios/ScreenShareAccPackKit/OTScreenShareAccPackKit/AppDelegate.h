f95b33272ef676105aac41af18fcf538223fe210
Jerónimo Valli <jeronimo.valli@gmail.com>
2016-10-04 19:15:37 -0300

    OTS-433: added Nullability compatibility

--- ios/ScreenShareAccPackKit/OTScreenShareAccPackKit/AppDelegate.h
+++ ios/ScreenShareAccPackKit/OTScreenShareAccPackKit/AppDelegate.h
@@ -6,6 +6,8 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
@@ -13,3 +15,4 @@
 
 @end
 
+NS_ASSUME_NONNULL_END
